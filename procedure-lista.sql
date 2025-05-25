38b885ed751d8a0a137a27ba968262453f5dbcff
Augusto <160799418+augustocaixeta@users.noreply.github.com>
2025-05-25 18:21:04 -0300

    .

--- procedure-lista.sql
+++ procedure-lista.sql
@@ -1,30 +1,25 @@
--- 1
-/*
-
 delimiter $$
 
 create procedure sp_aumento_salario(
-	in id_func int,
-	in taxa int,
-	out mensagem varchar(255)
+    in id_func int,
+    in taxa int,
+    out mensagem varchar(255)
 )
 begin
-	declare funcionario_existe int;
+    declare funcionario_existe int;
     select count(*) into funcionario_existe from funcionario as f where f.id_func = id_func;
     
     if funcionario_existe > 0 then
-		update funcionario as f set salario = salario * taxa where f.id_func = id_func;
-        set mensagem = concat('Salario do funcionario', id_func, ' atualizado com sucesso.');
-	else
-		set mensagem = concat('Funcionario', id_func, ' nao encontrado.');
+        update funcionario as f set salario = salario * taxa where f.id_func = id_func;
+        set mensagem = concat('Salario do funcionario ', id_func, ' atualizado com sucesso.');
+    else
+        set mensagem = concat('Funcionario ', id_func, ' nao encontrado.');
     end if;
 end $$
 
 delimiter ;
 
-*/
-
-delimiter $$
+-- Testes
 
 set @id_func = 1;
 set @taxa = 1.2;
@@ -32,5 +27,3 @@ set @mensagem = '';
 
 call sp_aumento_salario(@id_func, @taxa, @mensagem);
 select @mensagem;
-
-delimiter ;
